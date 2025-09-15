848caba94d3d46de177a1ea44e113c28abea91a6
Mtabel <162228744+Mtabel@users.noreply.github.com>
2025-09-14 23:55:34 -0700

    Updated to the averages.c
    Averages.c now works, but now I have to make it look pretty.

--- Lab_1/averages.c
+++ Lab_1/averages.c
@@ -1,40 +1,8 @@
 #include <stdio.h>
 
-float calc_average(int numbers[], int EvenOrOdd) {
+int get_input(int numbers[100])
+{
     int count = 0;
-    int opperation_count = 0;
-    int sum = 0;
-    if(numbers[count] == 0) {
-        printf("There is no average to compute.\n");
-        return -1;
-    }
-    if(EvenOrOdd == 0) // Even
-    {   
-        while (numbers[count] != 0) {
-            if(numbers[count] % 2 == 0) {
-                sum += numbers[count];
-                opperation_count++;
-            }
-            count++;
-        }
-    }
-    else if (EvenOrOdd == 1) // Odd
-    {
-        while (numbers[count] != 0) {
-            if(numbers[count] % 2 != 0) {
-                sum += numbers[count];
-                opperation_count++;
-            }
-            count++;
-        }
-    }
-    return (float)sum / opperation_count;
-}
-
-int main() {
-    int numbers[100];
-    int count = 0;
-    
     for(int i = 0; i < 100; i++) {
         printf("Enter integer %d (or 0 to stop): ", i + 1);
         scanf("%d", &numbers[i]);
@@ -43,16 +11,117 @@ int main() {
         }
         count++;
     }
+    printf("\n");
+    return count;
+}
+
+void get_even_odd_sums(int numbers[], int count, int even_sums[], int *even_count, int odd_sums[],int *odd_count)
+{
+    int even_tally = 0;
+    int odd_tally = 0;
+
+    for(int i = 0; i < count; i++)
+    {
+        int sum = 0;
+        int original_number = numbers[i];
+        if(original_number < 0) {
+            numbers[i] = -numbers[i]; // Make it positive for digit sum calculation
+        }
+        while(numbers[i] > 0)
+        {
+            sum += numbers[i] % 10;
+            numbers[i] /= 10;
+        }
+        if(sum % 2 == 0)
+        {
+            even_sums[even_tally] = original_number;
+            even_tally++;
+            (*even_count)++;
+        }
+        else
+        {
+            odd_sums[odd_tally] = original_number;
+            odd_tally++;
+            (*odd_count)++;
+        }
+
+
+    }
+    printf("Even Sums: %d, Odd Sums: %d", even_tally, odd_tally);
+    printf("\n");
+    printf("Even Count: %d, Odd Count: %d", *even_count, *odd_count);
+
+
+    
+}
+
+void take_average(int arr[], int size)
+{
+    if(size == 0) {
+        printf("No elements to compute average.\n");
+        return;
+    }
+    int sum = 0;
+    for(int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    double average = (double)sum / size;
+    printf("Average: %.2f\n", average);
+}
+
+void set_array(int arr[], int size, int value)
+{
+    for(int i = 0; i < size; i++) {
+        arr[i] = value;
+    }
+}
+
+void print_array(int arr[], int size)
+{
+    for(int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+int main() {
+    int numbers[100];
+    int even_sums[100];
+    int odd_sums[100];
+
+    int count = 0;
+    int even_count = 0;
+    int odd_count = 0;
+    
+
+    set_array(numbers, 100, 0);
+    set_array(even_sums, 100, 0);
+    set_array(odd_sums, 100, 0);
+
+    
+
+    count = get_input(numbers);
+
+    print_array(numbers, count);
+
+
+    get_even_odd_sums(numbers, count, even_sums, &even_count, odd_sums, &odd_count);
 
     printf("\n");
     
+    print_array(even_sums, even_count);
+    print_array(odd_sums, odd_count);
+
+    printf("tally of array count: %d\n", even_count);
+
+    take_average(even_sums, even_count);
+    take_average(odd_sums, odd_count);
+    
     if(count == 0) {
         printf("There is no average to compute.\n");
-        return 0;
+        return 1;
     }
-
-    printf("Average of input values whose digits sum up to an even number: %.2f\n", calc_average(numbers, 0));
-    printf("Average of input values whose digits sum up to an odd number: %.2f\n", calc_average(numbers, 1));
 
     return 0;
 }
