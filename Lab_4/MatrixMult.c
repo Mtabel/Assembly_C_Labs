bfe250dc70b673086205cc2a7d34f8875c623d04
Mtabel <matthew@tabelconstruction.com>
2025-10-09 19:55:45 -0700

    Lab4 Complete

--- Lab_4/MatrixMult.c
+++ Lab_4/MatrixMult.c
@@ -2,31 +2,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int** createArray(int n) {
+	int **arr = (int**)malloc(n * sizeof(int*));
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = (int*)malloc(n * sizeof(int));
+		for (int j = 0; j < n; j++) {
+			printf("Enter element [%d][%d]: ", i, j);
+			scanf("%d", &(*(*(arr + i) + j)));
+		}
+	}
+	return arr;
+}
+int** createArray_simple(int n) {
+	int **arr = (int**)malloc(n * sizeof(int*));
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = (int*)malloc(n * sizeof(int));
+		for (int j = 0; j < n; j++) {
+			(*(*(arr + i) + j)) = 0;
+		}
+	}
+	return arr;
+}
 int** matMult(int **a, int **b, int size) {
+	int **result = createArray_simple(size);
 	// (4) Implement your matrix multiplication here. 
 	// You will need to create a new matrix to store the product.
-
+	for(int i = 0; i < size; i++) {
+		for(int j = 0; j < size; j++) {
+			(*(*(result + i) + j)) = a[i][j] * b[i][j];
+		}
+	}
+	return result;
 }
 
-void printArray(int **arr, int n) {
-	// (2) Implement your printArray function here
+void printArray(int ** array, int size) {
+    // (5) Implement your printArr here:
+	for(int i = 0; i < size; i++) {
+		for(int j = 0; j < size; j++) {
+			printf("%d ", *(*(array + i) + j));
+		}
+		printf("\n");
+	}
 
 }
-
 int main() {
 	int n = 0;
 	int **matA, **matB, **matC;
 	// (1) Define 2 (n x n) arrays (matrices). 
-
+	printf("Enter size of matrices: ");
+	scanf("%d", &n);
+	matA = createArray(n);
+	matB = createArray(n);
 
 	// (3) Call printArray to print out the 2 arrays here.
-	
-	
-	// (5) Call matMult to multiply the 2 arrays here.
-	
-	
-	// (6) Call printArray to print out resulting array here.
+	printArray(matA, n);
+	printf("\n");
+	printArray(matB, n);
 
+	// (5) Call matMult to multiply the 2 arrays here.
+	matC = matMult(matA, matB, n);
+	printf("\n");	
+	// (6) Call printArray to print out resulting array here.
+	
+	printArray(matC, n);
 
     return 0;
 }
--- Lab_4/MemCast.c
+++ Lab_4/MemCast.c
@@ -16,6 +16,36 @@ int main() {
         four_c[i] = 'A' + i; // ASCII value of 'A' is 65 or 0x41 in Hex.
     
     // Add your code for the exercises here:
+	for(i = 0; i < 4; i++)
+		printf("%x ", four_c[i]);
 	
+	printf("%p\n", four_c);
+	//00000092A75FF7B0
+	//00000092A75FF7B0
+	printf("%p\n", &four_ints);
+	printf("\n");	
+		
+	// for ints
+	printf("Ints:\n");
+	for (i = 0; i < 4; i++)
+	{
+		printf("Value: %d\n", four_ints[i]);
+		printf("Pointer: %p\n", &four_ints[i]);
+		printf("Hex: %x\n", four_ints[i]);
+		printf("\n");
+
+	}
+	printf("\n");
+	for (i = 0; i < 4; i++)
+	{
+		printf("Value: %c\n", four_c[i]);
+		printf("Pointer: %p\n", &four_c[i]);
+		printf("Hex: %x\n", four_c[i]);
+		printf("\n");
+	}
+	
+
+	printf("\n");
+	printf("%x", 15);
 	return 0;
 }
