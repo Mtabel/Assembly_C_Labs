81b4a16731c379b546e5f44dc8b87e8b2e33bd7c
Mtabel <162228744+Mtabel@users.noreply.github.com>
2025-10-29 23:34:56 -0700

    Finished Asignment

--- Lab_5/myFirstMIPS.asm
+++ Lab_5/myFirstMIPS.asm
@@ -17,23 +17,28 @@ newline: .asciiz "\n"
 	#Store result into $t0
 	move $s0, $v0 #saves the input assigned to $v0 to $t0
 	
-	#print value
-	li $v0, 1 # print Integer instruction
-	move $a0, $s0
-	syscall
 	
 	#$t0 = $s0 
 	move $t0, $s0
 	#$t1 = $t0 - 7
+	addi $t1, $t0, -7
 	#$t2 = $t1 + $t0
+	add $t2, $t1, $t0
 	#$t3 = $t2 + 2
+	addi $t3, $t2, 2
 	#$t4 = $t3 + $t2
+	add $t4, $t3, $t2
 	#$t5 = $t4 - 28
+	addi $t5, $t4, -28
 	#$t6 = $t4 - $t5
+	sub $t6, $t4, $t5
 	#$t7 = $t6 + 12
+	addi $t7, $t6, 12
 	
 	
-	
+	move $a0, $t7
+	li $v0, 1
+	syscall
 	
 	li $v0, 10 # Load a 10 (service number for halt) into $v0.
 	syscall # The program ends because syscall reads the value of 10 in $v0.
