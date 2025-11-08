9b2c713c5d19bac771fca8dcb9627ff0a0c34e71
Mtabel <162228744+Mtabel@users.noreply.github.com>
2025-11-07 20:27:30 -0800

    blah

--- Lab_6/compare.s
+++ Lab_6/compare.s
@@ -25,17 +25,16 @@ n: .word 10
 	#Store result into $t0
 	move $s0, $v0 #saves the input assigned to $v0 to $t0
 	
-	
+#-------------------------------------------------
 		#Complete Less Than
 	#slt $t0, $s0, $t1 
 	#bne $t0, $zero, LT
-	#j finish
 
 		#Complete Greater than or equal to
 	#slt $t0, $s0, $t1 
 	#beq $t0, $zero, GE
 	#j finish
-	#Comment out above
+	#Comment out above ------------------------
 	#Make only greater than
 	slt $t0, $t1, $s0
 	bne $t0, $zero, GT
@@ -52,7 +51,7 @@ LT: #Less Than
 	j finish
 GE: #Greater than or Equal to
 	li $v0, 4 # Syscall 4 = print string
-	la $a0, str2 # loads address $a0 with prompt data
+	la $a0, str4 # loads address $a0 with prompt data
 	syscall
 	addi $t4, $t4, 2
 	j finish
